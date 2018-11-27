59b26cc78e354999c8e6355b40997ff721d32120
jaholt <holt.ja.halfways@gmail.com>
2018-11-27 15:53:56 +0000

    Started building the invoke method of the search controller, unfinished

--- src/Classes/Controllers/SearchController.php
+++ src/Classes/Controllers/SearchController.php
@@ -2,21 +2,28 @@
 
 namespace Portal\Controllers;
 
-
 use \Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Views\PhpRenderer;
 
 class SearchController
 {
     private $applicantModel;
+    private $renderer;
 
-    public function __construct(ApplicantModel $applicantModel)
+    public function __construct(ApplicantModel $applicantModel, PhpRenderer $renderer)
     {
         $this->applicantModel = $applicantModel;
+        $this->renderer = $renderer;
     }
 
-    public function __invoke(Request $request, Response $response)
+    public function __invoke(Request $request, Response $response, $args)
     {
-        
+        $searchResults = $request->getParsedBody();
+        $validatedSearchData = [
+            'name' => filter_var($searchResults['name'], FILTER_SANITIZE_STRING)
+        ];
+        $this->applicantModel->searchDatabaseByName($validatedSearchData);
+        return $this->renderer->render($response, 'searchResults.phtml', $args);
     }
 }
