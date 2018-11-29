4bdbafd925726790890b0c24b1da1eea3fd5bce2
Matt <matthew.tapley@hotmail.co.uk>
2018-11-29 17:00:28 +0000

    updated viewhelper

--- src/Classes/ViewHelpers/DisplayApplicantViewHelper.php
+++ src/Classes/ViewHelpers/DisplayApplicantViewHelper.php
@@ -19,14 +19,18 @@ class DisplayApplicantViewHelper
         $result = '';
         foreach ($applicants as $applicant) {
             if ($applicant instanceof ApplicantEntity) {
-                $result .= '<strong>Name:</strong> ' . $applicant->getName() . '<strong> Email:</strong> ' .
-                    $applicant->getEmail() . '<strong> Date Applied To:</strong> ' . $applicant->getCohortDate() . '<br>
-                            <div class="hiddenData"><strong>Phone Number:</strong> ' . $applicant->getPhoneNumber() .
-                    '<strong> Why Development?</strong> ' . $applicant->getWhyDev() . '<strong> Code Experience:</strong>
-                             ' . $applicant->getCodeExperience() . '<strong> How Did You Hear About Us:</strong> ' .
-                    $applicant->getHearAboutId() . '<strong> Eligible To Work In The Uk?</strong> ' . '<strong> 
-                             Over 18?</strong> ' . $applicant->getEighteenPlus() . '<strong> Finance?</strong> ' .
-                    $applicant->getFinance() . '<strong> Notes:</strong> ' . $applicant->getNotes() . '</div>';
+                $result .=  '<strong>Name: </strong>' . $applicant->getName() .
+                            '<strong> Email: </strong>' . $applicant->getEmail() .
+                            '<strong> Date Applied To: </strong>' . $applicant->getCohortDate() .
+                            '<br><div class="hiddenData"><strong>Phone Number: </strong>' . $applicant->getPhoneNumber() .
+                            '<br><strong> Why Development? </strong>' . $applicant->getWhyDev() .
+                            '<br><strong> Code Experience: </strong>' . $applicant->getCodeExperience() .
+                            '<br><strong> How Did You Hear About Us: </strong>' . $applicant->getHearAboutId() .
+                            '<br><strong> Eligible To Work In The Uk? </strong>' . $applicant->getEligible() .
+                            '<br><strong> Over 18? </strong>' . $applicant->getEighteenPlus() .
+                            '<br><strong> Finance? </strong>' . $applicant->getFinance() .
+                            '<br><strong> Notes: </strong>' . $applicant->getNotes() .
+                            '</div><button class="showData">Show More Data</button><br>';
             }
         }
         return ($result);
