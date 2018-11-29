2c738d4ab660ddd82d9c4e7a266f24293e7da3e0
Matt <matthew.tapley@hotmail.co.uk>
2018-11-29 16:53:15 +0000

    updated select statement in model and updated view helper

--- src/Classes/ViewHelpers/DisplayApplicantViewHelper.php
+++ src/Classes/ViewHelpers/DisplayApplicantViewHelper.php
@@ -19,7 +19,14 @@ class DisplayApplicantViewHelper
         $result = '';
         foreach ($applicants as $applicant) {
             if ($applicant instanceof ApplicantEntity) {
-                $result .= '<strong>Name:</strong> ' . $applicant->getName() . '<strong> Email:</strong> ' .  $applicant->getEmail() . '<strong> Date Applied To:</strong> ' . $applicant->getCohortDate() . '<br>';
+                $result .= '<strong>Name:</strong> ' . $applicant->getName() . '<strong> Email:</strong> ' .
+                    $applicant->getEmail() . '<strong> Date Applied To:</strong> ' . $applicant->getCohortDate() . '<br>
+                            <div class="hiddenData"><strong>Phone Number:</strong> ' . $applicant->getPhoneNumber() .
+                    '<strong> Why Development?</strong> ' . $applicant->getWhyDev() . '<strong> Code Experience:</strong>
+                             ' . $applicant->getCodeExperience() . '<strong> How Did You Hear About Us:</strong> ' .
+                    $applicant->getHearAboutId() . '<strong> Eligible To Work In The Uk?</strong> ' . '<strong> 
+                             Over 18?</strong> ' . $applicant->getEighteenPlus() . '<strong> Finance?</strong> ' .
+                    $applicant->getFinance() . '<strong> Notes:</strong> ' . $applicant->getNotes() . '</div>';
             }
         }
         return ($result);
